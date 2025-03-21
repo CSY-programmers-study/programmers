84fcc079c63f2644ece6de25721cd8c07f88a0d3
mor gan <4skadl@naver.com>
2025-03-22 00:38:18 +0900

    모루겠어욤

--- LV-0/l로만들기/morgan.js
+++ LV-0/l로만들기/morgan.js
@@ -1,3 +1,13 @@
-const { default: test } = require("node:test");
+function solution(myString) {
+    var mola = '';
+       for(let i = 0; i < myString.length; i++) { //반복문으로 반복함
+            if(myString[i]){
 
-test
+            }else {
+
+            }
+        }
+
+    console.log(solution('asdasdsasdasd'));
+
+}
